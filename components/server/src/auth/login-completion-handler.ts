03eca08f5f9548747bb7c43e26b9321a0857f922
jakobhero <jakob.herold.jh@gmail.com>
2022-03-08 11:30:03 +0000

    Identify User Subscription(s) at Login

--- components/server/src/auth/login-completion-handler.ts
+++ components/server/src/auth/login-completion-handler.ts
@@ -88,7 +88,7 @@ export class LoginCompletionHandler {
         if (authHost) {
             increaseLoginCounter("succeeded", authHost);
 
-            /** no await */ trackLogin(user, request, authHost, this.analytics).catch((err) =>
+            /** no await */ trackLogin(user, request, authHost, this.analytics, this.subscriptionService).catch((err) =>
                 log.error({ userId: user.id }, err),
             );
         }
