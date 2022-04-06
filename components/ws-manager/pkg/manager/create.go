668505e33a6ca932ef22fe997b067442b9f84e21
Pavel Tumik <18602811+sagor999@users.noreply.github.com>
2022-04-06 23:57:08 +0000

    wip

--- components/ws-manager/pkg/manager/create.go
+++ components/ws-manager/pkg/manager/create.go
@@ -516,6 +516,8 @@ func (m *Manager) createDefiniteWorkspacePod(startContext *startWorkspaceContext
 
 		case api.WorkspaceFeatureFlag_NOOP:
 
+		case api.WorkspaceFeatureFlag_PERSISTENT_VOLUME_CLAIM:
+
 		default:
 			return nil, xerrors.Errorf("unknown feature flag: %v", feature)
 		}
