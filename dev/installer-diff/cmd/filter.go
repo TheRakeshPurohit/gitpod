f20408a0ac7902e20c59b6e0229df24897e22239
Andrew Farries <andrew@gitpod.io>
2022-04-08 11:13:56 +0000

    Pretty print json fields in configmap data

--- dev/installer-diff/cmd/filter.go
+++ dev/installer-diff/cmd/filter.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"strings"
 
 	// yaml "gopkg.in/yaml.v2"
 
@@ -58,7 +59,6 @@ func filterJson() {
 		log.Fatalf("failed to unmarshal json: %s", err)
 	}
 
-	log.Printf("read %d items from stdin\n", len(objs))
 	// TODO gather all items in a list
 	// TODO to sorting based on GetKind/GetName
 	// TODO do filtering of labels on the generic unstructured objects (GetLabels/SetLabels)
@@ -68,30 +68,31 @@ func filterJson() {
 }
 
 func handle(obj unstructured.Unstructured) {
-
 	id := fmt.Sprintf("%s:%s", obj.GetKind(), obj.GetName())
+
+	fmt.Printf("--%s:%s\n", strings.ToUpper(obj.GetKind()), obj.GetName())
 
 	switch id {
 	case "ConfigMap:content-service-config":
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
 		assertNoError(err)
-		fmt.Printf("config.json:\n%s", cm.Data["config.json"])
+		printJsonDataFields(cm, "config.json")
 	case "ConfigMap:auth-providers-config":
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
 		assertNoError(err)
-		fmt.Printf("auth-providers.json:\n%s", cm.Data["auth-providers.json"])
+		printJsonDataFields(cm, "auth-providers.json")
 	case "ConfigMap:blobserve-config":
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
 		assertNoError(err)
-		fmt.Printf("config.json:\n%s", cm.Data["config.json"])
+		printJsonDataFields(cm, "config.json")
 	case "ConfigMap:image-builder-mk3-config":
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
 		assertNoError(err)
-		fmt.Printf("image-builder.json:\n%s", cm.Data["image-builder.json"])
+		printJsonDataFields(cm, "image-builder.json")
 	case "ConfigMap:proxy-config":
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
@@ -104,7 +105,7 @@ func handle(obj unstructured.Unstructured) {
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
 		assertNoError(err)
-		fmt.Printf("config.json:\n%s", cm.Data["config.json"])
+		printJsonDataFields(cm, "config.json")
 	case "ConfigMap:restarter-scripts":
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
@@ -114,70 +115,84 @@ func handle(obj unstructured.Unstructured) {
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
 		assertNoError(err)
-		fmt.Printf("config.json:\n%s", cm.Data["config.json"])
+		printJsonDataFields(cm, "config.json")
 	case "ConfigMap:workspace-template":
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
 		assertNoError(err)
-		fmt.Printf("default.yaml:\n%s", cm.Data["default.yaml"])
-		fmt.Printf("imagebuild.yaml:\n%s", cm.Data["imagebuild.yaml"])
-		fmt.Printf("prebuild.yaml:\n%s", cm.Data["prebuild.yaml"])
-		fmt.Printf("probe.yaml:\n%s", cm.Data["probe.yaml"])
-		fmt.Printf("regular.yaml:\n%s", cm.Data["regular.yaml"])
+		printJsonDataFields(cm,
+			"default.yaml",
+			"imagebuild.yaml",
+			"prebuild.yaml",
+			"probe.yaml",
+			"regular.yaml",
+		)
 	case "ConfigMap:ws-daemon-config":
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
 		assertNoError(err)
-		fmt.Printf("config.json:\n%s", cm.Data["config.json"])
+		printJsonDataFields(cm, "config.json")
 	case "ConfigMap:ws-manager-bridge-config":
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
 		assertNoError(err)
-		fmt.Printf("ws-manager-bridge.json:\n%s", cm.Data["ws-manager-bridge.json"])
+		printJsonDataFields(cm, "ws-manager-bridge.json")
 	case "ConfigMap:ws-manager-config":
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
 		assertNoError(err)
-		fmt.Printf("config.json:%s\n", cm.Data["config.json"])
+		printJsonDataFields(cm, "config.json")
 	case "ConfigMap:ws-proxy-config":
 		var cm *corev1.ConfigMap
 		err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &cm)
 		assertNoError(err)
-		fmt.Printf("config.json:%s\n", cm.Data["config.json"])
-	// case *appsv1.DaemonSet:
-	// 	fmt.Printf("object is a daemonset: %s\n", v.Name)
-	// case *appsv1.StatefulSet:
-	// 	fmt.Printf("object is a daemonset: %s\n", v.Name)
-	// case *corev1.ConfigMap:
-	// 	fmt.Printf("object is a configmap: %s\n", v.Name)
-	// case *corev1.ServiceAccount:
-	// 	fmt.Printf("object is a serviceaccount: %s\n", v.Name)
-	// case *corev1.Secret:
-	// 	fmt.Printf("object is a secret: %s\n", v.Name)
-	// case *corev1.Service:
-	// 	fmt.Printf("object is a service: %s\n", v.Name)
-	// case *corev1.ResourceQuota:
-	// 	fmt.Printf("object is a resourcequota: %s\n", v.Name)
-	// case *batchv1.Job:
-	// 	fmt.Printf("object is a job: %s\n", v.Name)
-	// case *batchv1.CronJob:
-	// 	fmt.Printf("object is a cronjob: %s\n", v.Name)
-	// case *rbacv1.ClusterRole:
-	// 	fmt.Printf("object is a clusterrole: %s\n", v.Name)
-	// case *rbacv1.ClusterRoleBinding:
-	// 	fmt.Printf("object is a clusterrolebinding: %s\n", v.Name)
-	// case *rbacv1.Role:
-	// 	fmt.Printf("object is a role: %s\n", v.Name)
-	// case *rbacv1.RoleBinding:
-	// 	fmt.Printf("object is a rolebinding: %s\n", v.Name)
-	// case *policyv1beta1.PodSecurityPolicy:
-	// 	fmt.Printf("object is a podsecuritypolicy: %s\n", v.Name)
-	// case *policyv1.PodDisruptionBudget:
-	// 	fmt.Printf("object is a poddisruptionbudget: %s\n", v.Name)
-	// case *networkingv1.NetworkPolicy:
-	// 	fmt.Printf("object is a networkpolicy: %s\n", v.Name)
-	default:
-		fmt.Printf("\nunhandled object kind: %s\n", id)
+		printJsonDataFields(cm, "config.json")
+		// case *appsv1.DaemonSet:
+		// 	fmt.Printf("object is a daemonset: %s\n", v.Name)
+		// case *appsv1.StatefulSet:
+		// 	fmt.Printf("object is a daemonset: %s\n", v.Name)
+		// case *corev1.ConfigMap:
+		// 	fmt.Printf("object is a configmap: %s\n", v.Name)
+		// case *corev1.ServiceAccount:
+		// 	fmt.Printf("object is a serviceaccount: %s\n", v.Name)
+		// case *corev1.Secret:
+		// 	fmt.Printf("object is a secret: %s\n", v.Name)
+		// case *corev1.Service:
+		// 	fmt.Printf("object is a service: %s\n", v.Name)
+		// case *corev1.ResourceQuota:
+		// 	fmt.Printf("object is a resourcequota: %s\n", v.Name)
+		// case *batchv1.Job:
+		// 	fmt.Printf("object is a job: %s\n", v.Name)
+		// case *batchv1.CronJob:
+		// 	fmt.Printf("object is a cronjob: %s\n", v.Name)
+		// case *rbacv1.ClusterRole:
+		// 	fmt.Printf("object is a clusterrole: %s\n", v.Name)
+		// case *rbacv1.ClusterRoleBinding:
+		// 	fmt.Printf("object is a clusterrolebinding: %s\n", v.Name)
+		// case *rbacv1.Role:
+		// 	fmt.Printf("object is a role: %s\n", v.Name)
+		// case *rbacv1.RoleBinding:
+		// 	fmt.Printf("object is a rolebinding: %s\n", v.Name)
+		// case *policyv1beta1.PodSecurityPolicy:
+		// 	fmt.Printf("object is a podsecuritypolicy: %s\n", v.Name)
+		// case *policyv1.PodDisruptionBudget:
+		// 	fmt.Printf("object is a poddisruptionbudget: %s\n", v.Name)
+		// case *networkingv1.NetworkPolicy:
+		// 	fmt.Printf("object is a networkpolicy: %s\n", v.Name)
+		// default:
+		// 	fmt.Printf("\nunhandled object kind: %s\n", id)
+	}
+}
+
+func printJsonDataFields(cm *corev1.ConfigMap, fields ...string) {
+	for _, field := range fields {
+		fmt.Printf("--%s\n", field)
+		var m interface{}
+		err := json.Unmarshal([]byte(cm.Data[field]), &m)
+		assertNoError(err)
+		b, err := json.MarshalIndent(m, "", " ")
+		assertNoError(err)
+		fmt.Println(string(b))
 	}
 }
 
